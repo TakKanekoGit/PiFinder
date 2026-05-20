97c74971ab42a83420f9f965b3c3c48e55d2d114
TakKanekoGit <>
2026-05-20 11:03:05 +0100

    Reads in gyro and accel datga

--- python/PiFinder/pointing_model/imu_record_gyro.py
+++ python/PiFinder/pointing_model/imu_record_gyro.py
@@ -14,23 +14,30 @@ import json
 from pathlib import Path
 import time
 #import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ImuSimple:
     def __init__(self):
         i2c = board.I2C()
         self.sensor = adafruit_bno055.BNO055_I2C(i2c)
-        self.sensor.mode = adafruit_bno055.IMUPLUS_MODE
-        # self.sensor.mode = adafruit_bno055.NDOF_MODE
+        self.sensor.mode = adafruit_bno055.ACCGYRO_MODE
 
-        self.calibration = 0
+        self.gyro = None  # Gyroscope data in rad/s
+        self.accel = None  # Accelerometer data in m/s^2
+        self.quat = None  # Quaternion data quaternion.quaternion(w, x, y, z)
+
+        #self.gyro_calibration_status = 0
+        #self.acc_calibration_status = 0
   
-        self.last_sample_time = time.time()
+        self.last_sample_time = time.time()  # Last time stamp when data was read from the IMU (but not necessarily valid and stored)
 
         # Calibration settings
-        self.imu_sample_frequency = 100.0 #30.0
+        self.imu_sample_frequency = 30.0
         self.imu_sample_period = 1 / self.imu_sample_frequency
 
-    def update(self):
+    def update(self) -> bool:
         ''' 
         Reads in the quaternion from the IMU. Returns True if a new valid
         sample is available. 
@@ -39,26 +46,38 @@ class ImuSimple:
         timestamp = time.time()
         if timestamp - self.last_sample_time < self.imu_sample_period:
             return False  # Not the end of the full sampling period
-
-        # adafruit_bno055 returns quaternion convention (w, x, y, z)
-        quat = self.sensor.quaternion  # Returns tuple so won't be over-written
         self.last_sample_time = timestamp
 
-        # Check IMU calibration status
-        self.calibration = self.sensor.calibration_status[1]
-        if self.calibration == 0:
-            # logger.warning("NOIMU CAL")
-            return False  # IMU not calibrated
+        accel = self.sensor.acceleration  # Acceleration in m/s^2
+        gyro = self.sensor.gyro  # Gyroscope in rad/s
 
-        if quat[0] is None:
+        """
+        # Check BNO055's internal calibration status
+        _, self.gyro_calibration_status, self.acc_calibration_status , _ \
+            = self.sensor.calibration_status  # (sys, gyro, accel, mag)
+        print(self.sensor.calibration_status)
+        if self.gyro_calibration_status == 0:
+            logger.warning("IMU: Gyro not calibrated")
+            return False  # Gyro not calibrated
+        # TODO: Check accelerometer cal status when it's used in future
+        """
+
+        if gyro[0] is None:
             # logger.warning("IMU: Failed to get sensor values")
             return False  # Failed to get sensor values
 
         # Valid sample obtained
-        self.quat = quat  # Scalar-first quaternion: (w, x, y, z)
+        self.gyro = gyro  # Gyroscope data in rad/s
+        self.accel = accel  # Accelerometer data in m/s^2
         self.timestamp = timestamp
 
         return True # New sample available
+
+    def sleep(self):
+        ''' Sleep for the remaining time in the sampling period '''
+        sleep_time = self.imu_sample_period - (time.time() - self.last_sample_time)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
 
 
 class RecordDataStream:
@@ -140,33 +159,43 @@ class RecordDataStream:
         self.buffer = []  # Flush the buffer
 
 
-def imu_monitor():
-    dir = Path("~/PiFinder_data/telemetry")
-    if not Path(dir).exists():
-        print(f"Directory {dir} does not exist. Creating it.")
-        Path(dir).mkdir(parents=True, exist_ok=True)
-    time_str = time.strftime("%Y%m%d%H%M")
+def imu_monitor(save_data=True):
+
+    if save_data:
+        dir = Path("~/PiFinder_data/telemetry")
+        if not Path(dir).exists():
+            print(f"Directory {dir} does not exist. Creating it.")
+            Path(dir).mkdir(parents=True, exist_ok=True)
+        time_str = time.strftime("%Y%m%d%H%M")
+
+        record = RecordDataStream(path=dir / f"{time_str}-imu_recording.jsonl")
+    else:
+        record = None
 
     imu = ImuSimple()
-    record = RecordDataStream(path=dir / f"{time_str}-imu_recording.jsonl")
-    max_samples = 20
+    max_samples = 100
     update_interval = 500
-
 
     n_samples = 0
     while True:
         if imu.update():
-            #print(
-            #    f"IMU: quat={imu.quat}, time={imu.timestamp:.3f}"
-            #)
-            record.store_imu_quaternion(imu.timestamp, imu.quat)
+            if save_data:
+                record.store_imu_quaternion(imu.timestamp, imu.quat)
+            else:
+                print(
+                    f"IMU: gyro={imu.gyro}, acc={imu.accel}, time={imu.timestamp:.3f}"
+                )
+
             n_samples += 1
             if n_samples % update_interval == 0:
                 print(f"Recorded {n_samples} IMU samples...")
             if n_samples >= max_samples:
-                record.stop_recording()
+                if save_data:
+                    record.stop_recording()
                 break
+        else:
+            imu.sleep()  # Sleep for the remaining time in the sampling period
 
 
 if __name__ == "__main__":
-    imu_monitor()
+    imu_monitor(save_data=False)
