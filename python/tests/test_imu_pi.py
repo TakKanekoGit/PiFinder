b280d8c183235c8ae1217b8110e8f03b79af3774
Tak Kaneko <>
2026-05-26 23:07:54 +0200

    Fix pytest for when enable_accel=False

--- python/tests/test_imu_pi.py
+++ python/tests/test_imu_pi.py
@@ -6,10 +6,10 @@ import pytest
 
 from PiFinder.imu.imu_pi import Imu
 
-@pytest.fixture
-def setup_imu():
+@pytest.fixture(params=[True, False])
+def setup_imu(request):
     # This method will run once before all tests
-    return Imu(enable_accel=True, emulate=True)
+    return Imu(enable_accel=request.param, emulate=True)
 
 def test_init(setup_imu):
     # Test the initialization of the Imu class
@@ -19,8 +19,9 @@ def test_read_accel_gyro_from_imu(setup_imu):
     # Test the _read_accel_gyro_from_imu method
     timestamp, accel, gyro = setup_imu._read_accel_gyro_from_imu()
     assert isinstance(timestamp, float)
-    assert isinstance(accel, np.ndarray) and accel is not None
     assert isinstance(gyro, np.ndarray) and gyro is not None
+    if setup_imu.accel_enabled:
+        assert isinstance(accel, np.ndarray) and accel is not None
 
 def test_read_gyro_from_imu(setup_imu):
     # Test the _read_gyro_from_imu method
@@ -32,8 +33,9 @@ def test_read_raw_data(setup_imu):
     # Test the _read_raw_data method
     timestamp, accel, gyro = setup_imu._read_raw_data()
     assert isinstance(timestamp, float)
-    assert isinstance(accel, np.ndarray) and accel is not None
     assert isinstance(gyro, np.ndarray) and gyro is not None
+    if setup_imu.accel_enabled:
+        assert isinstance(accel, np.ndarray) and accel is not None
 
 def test_update(setup_imu):
     # Test the update method
